85ad54af5ffb49fc42971faa16138d7c464dd954
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2025-05-28 20:56:26 -0400

    style: Made wording clearer

--- MetronomeApp/src/app/ui/settings/practice_ui.rs
+++ MetronomeApp/src/app/ui/settings/practice_ui.rs
@@ -3,8 +3,8 @@ use eframe::egui::{self, Ui};
 
 pub fn practice_ui(app: &mut MyApp, ui: &mut Ui) {
     egui::Frame::group(ui.style()).show(ui, |ui| {
-        ui.label("Practice:");
+        ui.label("Excercise Length:");
         ui.separator();
-        ui.add(egui::Slider::new(&mut app.tempo_params.length, 1..=600).text("Practice Length"));
+        ui.add(egui::Slider::new(&mut app.tempo_params.length, 10..=600).text("Seconds"));
     });
 }
