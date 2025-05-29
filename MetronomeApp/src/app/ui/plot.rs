347c52ae2c3942b43b834234503440c3558a9fa1
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2025-05-28 21:17:11 -0400

    style: Change Plot Bounds

--- MetronomeApp/src/app/ui/plot.rs
+++ MetronomeApp/src/app/ui/plot.rs
@@ -6,8 +6,14 @@ use egui_plot::{Line, Plot, PlotPoints};
 pub fn draw_plot(ui: &mut Ui, points: &[[f64; 2]], params: TempoParams) {
     Plot::new("live_plot")
         .view_aspect(2.0)
-        .default_x_bounds(-10.0, params.length as f64 + 10.0)
-        .default_y_bounds(params.min as f64 - 10.0, params.max as f64 + 10.0)
+        // .default_x_bounds(-10.0, params.length as f64 + 10.0)
+        // .default_y_bounds(params.min as f64 - 10.0, params.max as f64 + 10.0)
+        .allow_zoom(false)
+        .allow_drag(false)
+        .allow_scroll(false)
+        .allow_boxed_zoom(false)
+        .x_axis_label("Time (s)")
+        .y_axis_label("Tempo (BPM)")
         .show(ui, |plot_ui| {
             let line = Line::new("Tempo", PlotPoints::from(points.to_vec()));
             plot_ui.line(line);
