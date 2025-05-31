fce512a3ec52549a15b211d6255a158ff6383133
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2025-05-31 17:40:39 -0400

    feat: Add Fold Out Tab

--- MetronomeApp/src/app/ui/layout.rs
+++ MetronomeApp/src/app/ui/layout.rs
@@ -3,24 +3,59 @@ use crate::app::{MyApp, logic};
 use eframe::egui::{self, Context, Ui};
 
 pub fn layout(app: &mut MyApp, ctx: &Context) {
-    egui::SidePanel::left("settings")
-        .resizable(false)
-        .show(ctx, |ui| {
-            settings_ui(app, ui);
-        });
+    settings_ui(app, ctx);
+
+    if app.playing {
+        egui::SidePanel::right("info")
+            .resizable(false)
+            .show(ctx, |ui| {
+                settings::info_ui(app, ui);
+                settings::play_ui(app, ui);
+            });
+    }
 
     egui::CentralPanel::default().show(ctx, |ui| {
         main_ui(app, ui);
     });
 }
 
-fn settings_ui(app: &mut MyApp, ui: &mut Ui) {
+fn settings_ui(app: &mut MyApp, ctx: &Context) {
+    // Paused
     if !app.playing {
-        settings::practice_ui(app, ui);
-        settings::tempo_ui(app, ui);
+        egui::SidePanel::left("settings")
+            .resizable(false)
+            .show(ctx, |ui| {
+                settings::practice_ui(app, ui);
+                settings::tempo_ui(app, ui);
 
-        settings::growth_ui(app, ui);
-        settings::sound_ui(app, ui);
+                settings::growth_ui(app, ui);
+                settings::sound_ui(app, ui);
+            });
+    } else {
+        // Unpaused
+        let tab_size = 25.0;
+
+        egui::SidePanel::left("settings")
+            .resizable(false)
+            .frame(egui::Frame {
+                fill: ctx.style().visuals.window_fill(), // Matches background
+                inner_margin: egui::Margin::same(0),
+                outer_margin: egui::Margin::same(0),
+                ..Default::default()
+            })
+            .max_width(tab_size)
+            .show(ctx, |ui| {
+                ui.horizontal_centered(|ui| {
+                    ui.add(
+                        egui::Image::new(egui::include_image!("../../../assets/images/tab.png"))
+                            .fit_to_exact_size(egui::Vec2 {
+                                x: (tab_size),
+                                y: (tab_size),
+                            })
+                            .maintain_aspect_ratio(true),
+                    );
+                });
+            });
     }
 }
 
@@ -29,10 +64,11 @@ fn main_ui(app: &mut MyApp, ui: &mut Ui) {
         logic::tempo::calculate_tempo(app);
 
         settings::plot_ui(app, ui);
-
-        ui.horizontal(|ui: &mut Ui| {
-            settings::play_ui(app, ui);
-            settings::info_ui(app, ui);
-        });
+        if !app.playing {
+            ui.horizontal(|ui: &mut Ui| {
+                settings::play_ui(app, ui);
+                settings::info_ui(app, ui);
+            });
+        }
     });
 }
