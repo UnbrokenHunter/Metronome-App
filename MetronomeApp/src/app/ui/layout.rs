3fae5cc033a6414928a4f8c07a6b33f5db64a139
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2025-06-07 19:26:57 -0400

    feat: Add Switching Menus

--- MetronomeApp/src/app/ui/layout.rs
+++ MetronomeApp/src/app/ui/layout.rs
@@ -38,30 +38,38 @@ fn settings_ui(app: &mut AppData, ctx: &Context) {
     egui::SidePanel::left("settings")
         .resizable(false)
         .show(ctx, |ui| {
-            egui::Frame::group(ui.style()).show(ui, |ui| {
-                ui.label(RichText::new(format!("BPM: {:.2} BPM", app.runtime.tempo)).size(45.0));
-                ui.separator();
+            if app.runtime.menu == Menus::Metronome {
+                egui::Frame::group(ui.style()).show(ui, |ui| {
+                    ui.label(
+                        RichText::new(format!("BPM: {:.2} BPM", app.runtime.tempo)).size(45.0),
+                    );
+                    ui.separator();
 
-                ScrollArea::vertical().show(ui, |ui| {
-                    settings::practice_ui(app, ui);
-                    settings::tempo_ui(app, ui);
+                    ScrollArea::vertical().show(ui, |ui| {
+                        settings::practice_ui(app, ui);
+                        settings::tempo_ui(app, ui);
 
-                    settings::growth_ui(app, ui);
-                    settings::sound_ui(app, ui);
+                        settings::growth_ui(app, ui);
+                        settings::sound_ui(app, ui);
+                    });
                 });
-            });
+            } else if app.runtime.menu == Menus::Logs {
+            }
         });
 }
 
 fn main_ui(app: &mut AppData, ui: &mut Ui) {
     ui.vertical_centered(|ui| {
-        logic::tempo::calculate_tempo(app);
+        if app.runtime.menu == Menus::Metronome {
+            logic::tempo::calculate_tempo(app);
 
-        settings::parameters_ui(app, ui);
-        settings::plot_ui(app, ui);
-        ui.horizontal(|ui: &mut Ui| {
-            settings::play_ui(app, ui);
-            settings::info_ui(app, ui);
-        });
+            settings::parameters_ui(app, ui);
+            settings::plot_ui(app, ui);
+            ui.horizontal(|ui: &mut Ui| {
+                settings::play_ui(app, ui);
+                settings::info_ui(app, ui);
+            });
+        } else if app.runtime.menu == Menus::Logs {
+        }
     });
 }
