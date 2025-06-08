d11de043da1b29d3feadd9ebb5165acef9456ed2
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2025-06-08 12:34:02 -0400

    feat: Add Settings UI

--- MetronomeApp/src/app/ui/settings/log_settings_ui.rs
+++ MetronomeApp/src/app/ui/settings/log_settings_ui.rs
@@ -7,10 +7,24 @@ pub fn log_settings_ui(app: &mut AppData, ui: &mut Ui) {
         ui.heading("Log Settings");
         ui.separator();
 
+        ui.toggle_value(&mut app.settings.save_logs, "Save Logs");
+
+        ui.toggle_value(&mut app.settings.save_plots, "Save Plots");
+
+        // Only Show Plot Granularity if they are being saved in the first place
+        if app.settings.save_plots {
+            ui.radio_value(&mut app.settings.plot_granularity, 0, "Low");
+            ui.radio_value(&mut app.settings.plot_granularity, 1, "Medium");
+            ui.radio_value(&mut app.settings.plot_granularity, 2, "High");
+            ui.radio_value(&mut app.settings.plot_granularity, 3, "Lossless");
+        }
+
         ui.label("Delete Logs");
         if ui
             .add_sized([ui.available_width(), 30.0], egui::Button::new("Trash"))
             .clicked()
-        {}
+        {
+            app.practice.logs.clear();
+        }
     });
 }
