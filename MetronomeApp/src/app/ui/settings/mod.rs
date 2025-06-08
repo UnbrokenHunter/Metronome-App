c549e431bb890195984cd9b39257186cc6a910b6
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2025-06-08 12:06:15 -0400

    feat: Add Settings Tabs

--- MetronomeApp/src/app/ui/settings/mod.rs
+++ MetronomeApp/src/app/ui/settings/mod.rs
@@ -1,4 +1,6 @@
 pub mod log_settings_ui;
 pub mod settings_panel_layout;
+pub mod settings_side_layout;
 
 pub use settings_panel_layout::settings_panel_layout;
+pub use settings_side_layout::settings_side_layout;
--- MetronomeApp/src/app/ui/settings/settings_panel_layout.rs
+++ MetronomeApp/src/app/ui/settings/settings_panel_layout.rs
@@ -1,8 +1,12 @@
 use crate::app::{AppData, ui::settings::log_settings_ui::log_settings_ui};
-use eframe::egui::Ui;
+use eframe::egui::{self, Ui};
 
 pub fn settings_panel_layout(app: &mut AppData, ui: &mut Ui) {
     ui.vertical_centered(|ui| {
-        log_settings_ui(app, ui);
+        if (app.runtime.menu_state == 0) {
+            log_settings_ui(app, ui);
+        } else if app.runtime.menu_state == 1 {
+            egui::Frame::group(ui.style()).show(ui, |ui| ui.label("Menu State 1"));
+        }
     });
 }
