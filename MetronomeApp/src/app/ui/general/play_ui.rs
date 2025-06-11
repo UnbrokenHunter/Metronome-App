41244ce44c5c3999c795cb1bbedaf24820fdaabb
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2025-06-11 13:43:51 -0400

    fix: Fix Accent Button Size

--- MetronomeApp/src/app/ui/general/play_ui.rs
+++ MetronomeApp/src/app/ui/general/play_ui.rs
@@ -3,24 +3,18 @@ use eframe::egui::{self, Ui};
 use crate::app::{AppData, logic::logs};
 
 pub fn play_ui(app: &mut AppData, ui: &mut Ui) {
-    let half_width: f32 = ui.available_width() / 2.0;
+    let size = [ui.available_width(), 30.0];
 
     ui.vertical(|ui| {
-        if ui
-            .add_sized([half_width, 30.0], egui::Button::new("Play"))
-            .clicked()
-        {
+        if ui.add_sized(size, egui::Button::new("Play")).clicked() {
             app.runtime.playing = !app.runtime.playing;
         }
-        if ui
-            .add_sized([half_width, 30.0], egui::Button::new("Reset"))
-            .clicked()
-        {
+        if ui.add_sized(size, egui::Button::new("Reset")).clicked() {
             logs::try_add_log(app);
             app.reset_metronome();
         }
         if ui
-            .add_sized([half_width, 30.0], egui::Button::new("Revert Defaults"))
+            .add_sized(size, egui::Button::new("Revert Defaults"))
             .clicked()
         {
             logs::try_add_log(app);
--- MetronomeApp/src/app/ui/graph/graph_layout.rs
+++ MetronomeApp/src/app/ui/graph/graph_layout.rs
@@ -1,15 +1,25 @@
 use crate::app::ui::{general, graph, parameters};
 use crate::app::{AppData, logic};
-use eframe::egui::Ui;
+use eframe::egui::{self, Ui};
 
 pub fn graph_layout(app: &mut AppData, ui: &mut Ui) {
+    let total_width = ui.available_width();
+    let plot_width = total_width * 1.0 / 2.0;
+    let label_width = total_width - plot_width;
+
     ui.vertical_centered(|ui| {
         logic::tempo::calculate_tempo(app);
 
         parameters::parameters_ui(app, ui);
         graph::plot_ui(app, ui);
         ui.horizontal(|ui: &mut Ui| {
-            general::play_ui(app, ui);
+            ui.allocate_ui_with_layout(
+                egui::vec2(label_width, ui.available_height()),
+                egui::Layout::top_down(egui::Align::Min),
+                |ui| {
+                    general::play_ui(app, ui);
+                },
+            );
             general::info_ui(app, ui);
         });
     });
