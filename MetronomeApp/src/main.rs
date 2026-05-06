14d9f50b18ca08b70ce3fa0ae3d6ebbb43a2dd63
Hunter <huntersfred@gmail.com>
2026-05-06 02:18:58 -0400

    feat(app): Display app version in title

--- MetronomeApp/src/main.rs
+++ MetronomeApp/src/main.rs
@@ -13,15 +13,17 @@ fn main() -> eframe::Result<()> {
     let icon = from_png_bytes(&icon_bytes).expect("Invalid PNG data");
 
     let options = eframe::NativeOptions {
-        viewport: eframe::egui::ViewportBuilder::default()
+        viewport: egui::ViewportBuilder::default()
             .with_inner_size([1000.0, 450.0])
             .with_resizable(true)
             .with_icon(icon),
         ..Default::default()
     };
 
+    let app_title = format!("Metronome! v{}", env!("CARGO_PKG_VERSION"));
+
     eframe::run_native(
-        "Metronome!",
+        &app_title,
         options,
         Box::new(|cc| {
             egui_extras::install_image_loaders(&cc.egui_ctx);
